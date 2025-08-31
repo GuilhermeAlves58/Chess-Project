2c788c01855de17b71c410d56eb85e09c58833a3
perkles <guilhermealves5194@gmail.com>
2025-08-31 19:56:44 -0300

    checkmate logic/Piece move count

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -19,7 +19,7 @@ public abstract class Piece {
     }
 
     public Boolean isThereAnyPossibleMove(){
-        Boolean[][] mat = possibleMoves();
+        boolean[][] mat = possibleMoves();
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat.length; j++) {
                 if (mat[i][j]){
